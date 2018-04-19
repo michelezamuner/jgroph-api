bb2772abc6fd4bac620674cd41965352866e7e24
Michele Zamuner <michelezamuner@gmail.com>
2018-04-19 08:26:45 +0200

    Improved error linting

--- src/main/java/net/slc/jgroph/api/infrastructure/ResponseException.java
+++ src/main/java/net/slc/jgroph/api/infrastructure/ResponseException.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class ResponseException extends IOException
 {
+    private static final long serialVersionUID = 1836955718940582006L;
+
     public ResponseException(final String message)
     {
         super(message);
